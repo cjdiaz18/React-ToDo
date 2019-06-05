b562bcb9e3e2a98fda423a843ce760202d43b397
Carlos Diaz <hola@cjdiaz.com>
2019-06-04 20:21:42 -0500

    Changing State of React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/**
+  Componentes en React
+  1.- Stateful Components
+    - Classes
+    - State
+    - Lifecycle Methods
+
+  2.- Stateless Components (Functional Components):
+    -Fuctions
+    -No State
+    -No Lifecycles Methods
+ **/
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      todos: []
+    };
+  }
+
+  updateValue = event => {
+    // this.state.value = value;
+    this.setState({ value: event.target.value });
+  };
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <React.Fragment>
+        <Typography variant="h5" align="center" gutterBottom>
+          ToDo App
+        </Typography>
+
+        <TextField
+          type="text"
+          placeholder="Start typing…"
+          margin="normal"
+          onChange={this.updateValue}
+          value={this.state.value}
+        />
+      </React.Fragment>
+    ); // JSX
+  }
 }
 
 export default App;
